34629934eb5d0233e281bab20e3f45d681cf47e3
Jure <jurejelic1405@gmail.com>
2022-03-05 00:05:34 +0100

    Dodano MyUserDetailsService i UserRepository.

--- src/main/java/com/autorapp/autorApp/controller/AutorController.java
+++ src/main/java/com/autorapp/autorApp/controller/AutorController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/autori")
+@RequestMapping({"/user/autori", "/admin/autori"})
 public class AutorController {
 
     private final AutorService autorService;
